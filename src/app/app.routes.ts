96bb6f7b681511fc78f99a72a603f21be5225b1a
alfabeat <williamunderh@gmail.com>
2025-08-21 03:08:34 +0100

    trying prerender fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,17 +11,18 @@ import { MemberDetailComponent } from './member-detail/member-detail.component';
 import { AddEventComponent } from './add-event/add-event.component';
 import { EventDetailComponent } from './event-detail/event-detail.component';
 import { LoginComponent } from './login/login.component';
+import { RenderMode } from '@angular/ssr';
 export const routes: Routes = [
     {path: 'nav', component: NavbarComponent, title: 'NavbarComponent'},
     { path: 'member', component: MemberListComponent, title: 'Member List' },
      {path: '', component: AboutComponent, title: 'About' },
      {path: 'event', component: EventPageComponent, title: 'event' },
-    {path: 'events/:id', component: EventDetailComponent, title: 'eventdetail' },
-    {path: 'events/:id/add', component: AddEventComponent, title: 'Add Event' },
-    {path: 'events/:id/edit', component: FormEventComponent, title: 'Edit Event' },
-     {path: 'members/:id', component: MemberDetailComponent, title: 'memberdetail' },
-     {path: 'members/:id/add', component: AddMemberComponent, title: 'Add Member' },
-     {path: 'members/:id/edit', component: MemberFormComponent, title: 'Edit Member' },
+    {path: 'events/:id', component: EventDetailComponent, title: 'eventdetail', data: { prerender: false } },
+    {path: 'events/:id/add', component: AddEventComponent, title: 'Add Event' , data: { prerender: false }},
+    {path: 'events/:id/edit', component: FormEventComponent, title: 'Edit Event' , data: { prerender: false }},
+     {path: 'members/:id', component: MemberDetailComponent, title: 'memberdetail' , data: { prerender: false }},
+     {path: 'members/:id/add', component: AddMemberComponent, title: 'Add Member' , data: { prerender: false }},
+     {path: 'members/:id/edit', component: MemberFormComponent, title: 'Edit Member' , data: { prerender: false }},
    {path: 'login', component: LoginComponent, title: 'Login' },
 
 ];
